7ecd23cf477ebd55c9f80877b600aaae3e851b16
Gravity021 <76179528+Gravity021@users.noreply.github.com>
2023-05-26 15:17:07 +0100

    Update NeoVim Windows config files

---
+++ nvim/windows/lua/options.lua
@@ -0,0 +1,42 @@
+-- Native NeoVim configurations
+function configureOpts()
+	local set = vim.opt
+
+	-- Misc
+	set.compatible = false				-- Disable compatibility to old-time vi
+
+	set.clipboard = unnamedplus			-- Use system clipboard
+	set.mouse = "a"						-- Enable mouse click
+	set.mouse = "v"						-- Paste with middle-click
+
+	-- Tab
+	set.tabstop = 4						-- Number of columns occupied by a tab
+	set.softtabstop = 4					-- See multiple spaces as tabstops so <BS> does the right thing
+	set.expandtab = true				-- Converts tabs to whitespace
+	set.shiftwidth = 4					-- Width for autoindents
+	set.autoindent = true				-- Indent a new line by the same ammount as the line just typed
+
+	-- Searching
+	set.incsearch = true				-- Incremental search
+	set.hlsearch = true					-- Highlight search
+	set.ignorecase = true				-- Case insensitive
+	vim.opt.smartcase = true            -- Override search case insensitivity if an uppercase is entered
+
+	-- UI
+	set.wrap = false
+	set.number = true					-- Add line numbers
+	set.cursorline = true				-- Highlight the current cursor line
+	set.cc = "80"						-- Set an 80 column border for good coding style
+	set.wildmode = longest, list 		-- Get bash-like tab completions
+	set.syntax = "on"					-- Enable syntax highlighting
+
+	set.showmatch = true				-- Show matching bracket
+	set.filetype.pluginindent = "on"	-- Allow auto-indenting depending on file type
+	set.filetype.plugin = "on"			-- Enable loading plugins for filetypes
+	set.ttyfast = true					-- Speed up scrolling
+
+	set.splitright = true				-- Open new split panes right
+	set.splitbelow = true				-- Open new split panes below
+
+	set.termguicolors = true			-- Keep consistant colouring with the terminal
+end
--- nvim/windows/lua/plugins.lua
+++ nvim/windows/lua/plugins.lua
@@ -1,17 +1,66 @@
+local packer = require("packer")
+
 -- NeoVim Plugins (using packer)
 function installPlugins()
-	return require("packer").startup(function(use)
-		use "wbthomason/packer.nvim"
+	return packer.startup(function(use)
+		use "wbthomason/packer.nvim"								-- Update packer from packer
 		
 		-- Colourschemes
-		use "dracula/vim"
+		--use "dracula/vim"
+		use 'folke/tokyonight.nvim'
 		
 		-- Tools
-		use "mhinz/vim-startify"
+		use {'glepnir/dashboard-nvim', 								-- Tool to change the startup interface
+			event = 'VimEnter', 
+			config = function() require('dashboard').setup {
+				theme = "hyper",
+				hide = {statusline = false},
+				config = {
+					shortcut = {
+						{desc = '󰊳 Update', group = '@property', action = 'source $MYVIMRC || PackerSync || MasonUpdate', key = 'u'}
+					}
+				}
+			} end, 
+			requires = {'nvim-tree/nvim-web-devicons'}
+		}
+		use {"folke/todo-comments.nvim",							-- Tool for highlighting todo comments
+			requires = "nvim-lua/plenary.nvim",
+			config = function() require("todo-comments").setup {}
+			end
+		}
+		--use {"folke/trouble.nvim",									-- Tool for 
+		--	requires = "nvim-tree/nvim-web-devicons",
+		--	config = function() require("trouble").setup {} end
+		--}
+		use {'nvim-telescope/telescope.nvim', 						-- Tool for 
+			branch = '0.1.x',
+			requires = {'nvim-lua/plenary.nvim'}
+		}
+		use {'nvim-lualine/lualine.nvim', 							-- Tool for configuring the statusline
+			requires = {'nvim-tree/nvim-web-devicons', opt = true},
+			config = function() require('lualine').setup {} end
+		}
+		use {"williamboman/mason.nvim"}								-- Tool for loading LSPs and DAPs
+		use {'williamboman/mason-lspconfig.nvim'}
+		
+		--use { 'codota/tabnine-nvim', run = "./dl_binaries.sh" }		-- Tabnine for code completion
+		
+		--use {'romgrk/barbar.nvim', 
+		--	requires = {
+		---		'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+		--		'nvim-tree/nvim-web-devicons' -- OPTIONAL: for file icons
+		--	}
+		--}
 		
 		-- Utils
-		use "ryanoasis/vim-devicons"
+		use {"windwp/nvim-autopairs", 								-- Utility for adding autopairs to parentheses
+			config = function() require("nvim-autopairs").setup {
+				ensure_installed = {"pylsp", "lua_ls"}
+			} end
+		}
+		use 'nvim-tree/nvim-web-devicons'							-- Utility for adding icons to text
+		use "xiyaowong/transparent.nvim"							-- Utility for making the background transparent
 		
-		require("packer").sync()
+		--packer.sync() -- Handled as part of "dashboard-nvim"
 	end)
 end
